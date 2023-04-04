72cd407b2830b7b37feaaf206c4d49c6cc31e163
Ha Anna <its.haanna@gmail.com>
2023-04-04 22:43:03 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import Note from "./components/Note"
+import { DragDropContext } from 'react-beautiful-dnd';
 
 
 function App() {
+  const onDragEnd: any = useCallback(() => {
+    console.log('hu')
+  }, []);
 
 
   return (
     <div className="dashboard">
+      <DragDropContext
+      onDragEnd={onDragEnd}
+    >
       <Note />
+    </DragDropContext>
     </div>
   )
 }
